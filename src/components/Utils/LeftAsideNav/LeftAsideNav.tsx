605b9eb05e59d49bb86f10c8d2341e579d3df7ff
JoshuaSiyame <josh2023decoded@gmail.com>
2023-09-12 17:55:58 +0300

    Left aside navigation

--- src/components/Utils/LeftAsideNav/LeftAsideNav.tsx
+++ src/components/Utils/LeftAsideNav/LeftAsideNav.tsx
@@ -1,16 +1,33 @@
 // import modules/packages
-import React from "react";
+import React, { useState } from "react";
 import "./LeftAsideNav.css";
 
 // LeftAsideNav component
 const LeftAsideNav = () => {
+    // use states to control left nav visibility
+    const [getStartedMenu, setGetStartedMenu] = useState<boolean>(false);
+    const [frameworksMenu, setFrameworksMenu] = useState<boolean>(false);
+    const [projects, setProjects] = useState<boolean>(false);
+
+    const handleGetStartedToggle = (): any =>{
+        setGetStartedMenu(!getStartedMenu);        
+    };
+
+    const handleFrameworkToggle = () =>{
+        setFrameworksMenu(!frameworksMenu);
+    };
+
+    const handleProjectsToggle = () =>{
+        setProjects(!projects);
+    }
+
     return (
         <aside id="left-nav-aside">
             <h4 className="main-container-section-header">Guides</h4>
             <div id="left-nav-aside-content-container">
                 <div className="left-nav-aside-content">
-                    <h4 className="aside-content-header">Get Started</h4>
-                    <ol id="" className="aside-content-list">
+                    <h4 className="aside-content-header" onClick={()=>handleGetStartedToggle()}>Get Started</h4>
+                    <ol id="get-started" className={ getStartedMenu? "aside-content-list-active": "aside-content-list"}>
                         <li className="aside-content-list-item">
                             <a href="" className="aside-content-list-item-link">ExpressJs Js project structure</a>
                         </li>
@@ -38,8 +55,8 @@ const LeftAsideNav = () => {
                     </ol>
                 </div>
                 <div className="left-nav-aside-content">
-                    <h4 className="aside-content-header">Frameworks</h4>
-                    <ol id="" className="aside-content-list">
+                    <h4 className="aside-content-header" onClick={()=>handleFrameworkToggle()}>Frameworks</h4>
+                    <ol id="frameworks" className={frameworksMenu? "aside-content-list-active" :"aside-content-list"}>
                         <li className="aside-content-list-item">
                             <a href="" className="aside-content-list-item-link">ExpressJs</a>
                         </li>
@@ -61,8 +78,8 @@ const LeftAsideNav = () => {
                     </ol>
                 </div>
                 <div className="left-nav-aside-content">
-                    <h4 className="aside-content-header">Projects</h4>
-                    <ol id="" className="aside-content-list">
+                    <h4 className="aside-content-header" onClick={()=>handleProjectsToggle()}>Projects</h4>
+                    <ol id="projects" className={projects? "aside-content-list-active" :"aside-content-list"}>
                         <li className="aside-content-list-item">
                             <a href="" className="aside-content-list-item-link">ExpressJs</a>
                         </li>
